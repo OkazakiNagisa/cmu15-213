f34a892f3f06939cb6983b97e34519ba2dbba6c9
nakido <nakidosama@outlook.com>
2023-11-19 00:06:03 +0800

    lab4 fin

--- labs/4.cachelab-handout/trans-my.c
+++ labs/4.cachelab-handout/trans-my.c
@@ -9,21 +9,21 @@ extern void trans(int M, int N, int A[N][M], int B[M][N]);
 
 int main(int argc, const char *argv[])
 {
-    int A[32][32] = {0};
-    int B[32][32] = {0};
+    int A[67][61] = {0};
+    int B[61][67] = {0};
     // randMatrix(32, 32, A);
     int a = 0;
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 67; i++)
     {
-        for (int j = 0; j < 32; j++)
+        for (int j = 0; j < 61; j++)
         {
             A[i][j] = ++a;
         }
     }
 
     // trans(32, 32, A, B);
-    transpose_submit(32, 32, A, B);
+    transpose_submit(61, 67, A, B);
 
-    printf("%s\n", is_transpose(32, 32, A, B) ? "true" : "false");
+    printf("%s\n", is_transpose(61, 67, A, B) ? "true" : "false");
     return 0;
 }
--- labs/4.cachelab-handout/trans.c
+++ labs/4.cachelab-handout/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,79 +6,134 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
-#include <stdio.h>
+ */
 #include "cachelab.h"
+#include <stdio.h>
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-    for (int i = 0; i + 3 < N; i += 4)
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    // hits:1765, misses:288, evictions:256
+    if (M == 32 && N == 32)
     {
-        for (int j = 0; j + 3 < M; j += 4)
+        for (int i = 0; i + 7 < N; i += 8)
         {
-            int a11 = A[i + 0][j + 0];
-            int a12 = A[i + 0][j + 1];
-            int a13 = A[i + 0][j + 2];
-            int a14 = A[i + 0][j + 3];
+            for (int j = 0; j + 7 < M; j += 8)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    int a1 = A[i + k][j + 0];
+                    int a2 = A[i + k][j + 1];
+                    int a3 = A[i + k][j + 2];
+                    int a4 = A[i + k][j + 3];
+                    int a5 = A[i + k][j + 4];
+                    int a6 = A[i + k][j + 5];
+                    int a7 = A[i + k][j + 6];
+                    int a8 = A[i + k][j + 7];
 
-            int a21 = A[i + 1][j + 0];
-            int a22 = A[i + 1][j + 1];
-            int a23 = A[i + 1][j + 2];
-            int a24 = A[i + 1][j + 3];
-
-            B[j + 0][i + 0] = a11;
-            B[j + 0][i + 1] = a21;
-
-            B[j + 1][i + 0] = a12;
-            B[j + 1][i + 1] = a22;
-
-            B[j + 2][i + 0] = a13;
-            B[j + 2][i + 1] = a23;
-
-            B[j + 3][i + 0] = a14;
-            B[j + 3][i + 1] = a24;
-
-            a11 = A[i + 2][j + 0];
-            a12 = A[i + 2][j + 1];
-            a13 = A[i + 2][j + 2];
-            a14 = A[i + 2][j + 3];
-
-            a21 = A[i + 3][j + 0];
-            a22 = A[i + 3][j + 1];
-            a23 = A[i + 3][j + 2];
-            a24 = A[i + 3][j + 3];
-
-            B[j + 0][i + 2] = a11;
-            B[j + 0][i + 3] = a21;
-
-            B[j + 1][i + 2] = a12;
-            B[j + 1][i + 3] = a22;
-
-            B[j + 2][i + 2] = a13;
-            B[j + 2][i + 3] = a23;
-
-            B[j + 3][i + 2] = a14;
-            B[j + 3][i + 3] = a24;
+                    B[j + 0][i + k] = a1;
+                    B[j + 1][i + k] = a2;
+                    B[j + 2][i + k] = a3;
+                    B[j + 3][i + k] = a4;
+                    B[j + 4][i + k] = a5;
+                    B[j + 5][i + k] = a6;
+                    B[j + 6][i + k] = a7;
+                    B[j + 7][i + k] = a8;
+                }
+            }
         }
+    }
+    // hits:6497, misses:1700, evictions:1668
+    else if (M == 64 && N == 64)
+    {
+        for (int i = 0; i + 3 < N; i += 4)
+        {
+            for (int j = 0; j + 3 < M; j += 4)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    int a1 = A[i + k][j + 0];
+                    int a2 = A[i + k][j + 1];
+                    int a3 = A[i + k][j + 2];
+                    int a4 = A[i + k][j + 3];
+
+                    B[j + 0][i + k] = a1;
+                    B[j + 1][i + k] = a2;
+                    B[j + 2][i + k] = a3;
+                    B[j + 3][i + k] = a4;
+                }
+            }
+        }
+    }
+    // hits:6092, misses:2087, evictions:2055
+    else if (M == 61 && N == 67)
+    {
+        int i;
+        int j;
+        for (i = 0; i + 7 < N; i += 8)
+        {
+            for (j = 0; j + 7 < M; j += 8)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    int a1 = A[i + k][j + 0];
+                    int a2 = A[i + k][j + 1];
+                    int a3 = A[i + k][j + 2];
+                    int a4 = A[i + k][j + 3];
+                    int a5 = A[i + k][j + 4];
+                    int a6 = A[i + k][j + 5];
+                    int a7 = A[i + k][j + 6];
+                    int a8 = A[i + k][j + 7];
+
+                    B[j + 0][i + k] = a1;
+                    B[j + 1][i + k] = a2;
+                    B[j + 2][i + k] = a3;
+                    B[j + 3][i + k] = a4;
+                    B[j + 4][i + k] = a5;
+                    B[j + 5][i + k] = a6;
+                    B[j + 6][i + k] = a7;
+                    B[j + 7][i + k] = a8;
+                }
+            }
+        }
+        for (int ii = i; ii < N; ii++)
+        {
+            for (int jj = 0; jj < M; jj++)
+            {
+                int tmp = A[ii][jj];
+                B[jj][ii] = tmp;
+            }
+        }
+        for (int ii = 0; ii < i; ii++)
+        {
+            for (int jj = j; jj < M; jj++)
+            {
+                int tmp = A[ii][jj];
+                B[jj][ii] = tmp;
+            }
+        }
+    }
+    else
+    {
+        void trans(int M, int N, int A[N][M], int B[M][N]);
+        trans(M, N, A, B);
     }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
@@ -86,13 +141,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -105,14 +161,13 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    // registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -121,13 +176,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
